5c835b51f8845c612cb9269bd6a2ab16a6f30679
Sengolda <79252176+Sengolda@users.noreply.github.com>
2021-05-11 18:54:11 +0530

    Create __init__.py

---
+++ python/module-example/MyModule/__init__.py
@@ -0,0 +1,8 @@
+
+from .MYMODULE import * # IMPORTING THE FILE
+
+__author__ = "Me"
+__title__ = "MyModule"
+__copyright__ = "Copyright {} 2021".format(__author__)
+__version__ = "0.1.0"
+__license__ = "MIT"
