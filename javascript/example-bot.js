b98f5e6f5e81fc4b830108112c8c6c212bb170ee
Sengolda <79252176+Sengolda@users.noreply.github.com>
2021-05-11 19:24:17 +0530

    Update example-bot.js

--- javascript/example-bot.js
+++ javascript/example-bot.js
@@ -9,13 +9,6 @@ const cooldowns = new Discord.Collection();
 
 client.on("ready", async () => {
     console.log(`${client.user.tag} is Online!`)
-    client.user
-   function randomStatus() {
-    let status = ["discord.gg/tca", "Ping for Prefix!", `over ${client.users.cache.size} Online Friends! <3`, `over ${client.guilds.cache.size} Servers! <3`]
-    let rstatus = Math.floor(Math.random() * status.length);
-
-    client.user.setActivity(status[rstatus], {type: "WATCHING"});
-    }; setInterval(randomStatus, 20000)
 });
 
 client.login(config.TOKEN)
