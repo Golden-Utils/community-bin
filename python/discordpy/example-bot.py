de97300cfced1a80a874dc5df32c999c94a339bc
Sengolda <79252176+Sengolda@users.noreply.github.com>
2021-05-11 18:45:26 +0530

    Create example-bot.py

---
+++ python/discordpy/example-bot.py
@@ -0,0 +1,24 @@
+# This is a very basic bot!
+import discord
+from discord.ext import commands
+import random
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def ping(ctx):
+  await ctx.send(bot.latency)
